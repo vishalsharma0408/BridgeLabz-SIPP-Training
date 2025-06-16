0a83d10c5b4a664cf87853ee94ad9992450edc66
vishalsharma0408 <157011509+vishalsharma0408@users.noreply.github.com>
2025-06-16 12:52:04 +0530

    Delete Java_Fundamentals/Question20.java

--- Java_Fundamentals/Question20.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;
-class Question20{
-    public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        float Celsius=sc.nextFloat();
-        float farenheitResult=(float)(Celsius*1.8)+32;
-        System.out.println("The"+" "+Celsius+" "+"celsius is"+" "+farenheitResult+" "+"fahrenheit");
-    }
-}
