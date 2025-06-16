b42f36c28c6ad975b730d9e614a27cce2aac8f2f
vishalsharma0408 <157011509+vishalsharma0408@users.noreply.github.com>
2025-06-16 12:52:21 +0530

    Delete Java_Fundamentals/Question22.java

--- Java_Fundamentals/Question22.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;
-class Question22{
-    public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        int numberOfStudents=sc.nextInt();
-        int maximum_number_of_possible_handshakes=(numberOfStudents*(numberOfStudents -1))/2;
-        System.out.println("the number of possible handshakes"+" "+maximum_number_of_possible_handshakes);
-    }
-}
