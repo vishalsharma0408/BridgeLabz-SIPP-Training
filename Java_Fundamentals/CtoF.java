25cc62084b1df77c6bc2c215bc6866403753dc8e
vishalsharma0408 <vishal45sharma00@gmail.com>
2025-06-16 10:31:44 +0530

    Remove all java files

--- Java_Fundamentals/CtoF.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-class CtoF{
-    public static void main(String[] args){
-	Scanner sc = new Scanner(System.in);
-	int celcius=sc.nextInt();
-	int Fahrenheit=(celcius * 9/5) + 32;
-	System.out.println("Converted Temp:" + Fahrenheit);
-	}
-}
